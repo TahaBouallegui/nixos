a4c77611da7ca3366ce0e361cdac553a8bddac0d
Za3ter <ahmedtbou@tutamail.com>
2026-05-14 20:54:04 +0200

    sunshine fix test

--- modules/features/sunshine.nix
+++ modules/features/sunshine.nix
@@ -8,16 +8,13 @@
     pkgs,
     ...
   }: {
+    
     boot.kernelParams = [ "video=DP-1:1920x1080@60" ];
     
     services.sunshine = {
       enable = true;
-      autoStart = true;
+      autoStart = true; 
       openFirewall = true;
-      package = pkgs.sunshine.override {
-        cudaSupport = true;
-        cudaPackages = pkgs.cudaPackages;
-      };
     };
   };
 }
