9ed72ae8260e450dd7eb54dcd6561b5db550643c
Za3ter <ahmedtbou@tutamail.com>
2026-05-16 22:17:07 +0200

    time sync

--- modules/base/set-time.nix
+++ modules/base/set-time.nix
@@ -20,7 +20,7 @@
       ];
       serviceConfig = {
         Type = "oneshot";
-        ExecStart = "${pkgs.coreutils}/bin/date -s '2026-05-16 15:12'";
+        ExecStart = "${pkgs.coreutils}/bin/date -s '2026-05-16 16:53'";
       };
     };
 
