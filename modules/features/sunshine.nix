1029317f8c7d6a3ae821593af9d4470d4a5d53c3
Za3ter <ahmedtbou@tutamail.com>
2026-05-17 13:23:52 +0200

    wayland & cage

--- modules/features/sunshine.nix
+++ modules/features/sunshine.nix
@@ -9,61 +9,31 @@
     ...
   }: {
 
-    # X11 display with i3 and auto‑login
-    services.xserver = {
-      enable = true;
-      desktopManager.xfce.enable = true;
-      displayManager.lightdm.enable = true;
-      displayManager.autoLogin = {
+    # auto‑login
+    services.greetd = {
         enable = true;
-        user = "za3ter";
-      };
+        settings = {
+            defualt_session = {
+                command = "${pkgs.cage}/bin/cage -s -- ${pkgs.steam}/bin/steam -bigpicture";
+                user = "za3ter";
+            };
+        };
     };
-    environment.etc."X11/xorg.conf.d/headless-virtual-display.conf".text = ''
-  Section "ServerLayout"
-      Identifier "TwinLayout"
-      Screen 0 "metaScreen" 0 0
-  EndSection
+    # No need for Xorg
+    services.xserver.enable = false;
 
-  Section "Monitor"
-      Identifier "Monitor0"
-      Option "Enable" "true"
-  EndSection
-
-  Section "Device"
-      Identifier "Card0"
-      Driver "nvidia"
-      VendorName "NVIDIA Corporation"
-      Option "MetaModes" "1920x1080"
-      Option "ConnectedMonitor" "DP-0"
-      Option "ModeValidation" "NoDFPNativeResolutionCheck,NoVirtualSizeCheck,NoMaxPClkCheck,NoHorizSyncCheck,NoVertRefreshCheck,NoWidthAlignmentCheck"
-  EndSection
-
-  Section "Screen"
-      Identifier "metaScreen"
-      Device "Card0"
-      Monitor "Monitor0"
-      DefaultDepth 24
-      Option "TwinView" "True"
-      SubSection "Display"
-          Modes "1920x1080"
-      EndSubSection
-  EndSection
-'';
 users.users.za3ter = {
   extraGroups = [ "uinput" ];
+  isNormalUser = true;
 };
     hardware.uinput.enable = true;
     
     services.sunshine = {
       enable = true;
       autoStart = true; 
+      capSysAdmin = true;
       openFirewall = true;
     };
-    environment.systemPackages = with pkgs; [
-        xrandr
-        x11vnc
-    ];
 
     programs.steam = {
         enable = true;
