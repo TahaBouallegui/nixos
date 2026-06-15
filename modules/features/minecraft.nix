d505c242f9fd2d1b0558f2eb7e6e47bb06591997
Za3ter <ahmedtbou@tutamail.com>
2026-06-15 22:52:17 +0200

    minecraft start

--- modules/features/minecraft.nix
+++ modules/features/minecraft.nix
@@ -14,7 +14,7 @@
 
         servers.my-fabric-server = {
           enable = true;
-          package = pkgs.fabricServers.fabric-1_21;
+          package = pkgs.fabricServers.fabric-1_21_11;
           serverProperties = {
             server-port = 25565;
             difficulty = "normal";
@@ -26,10 +26,17 @@
 
           jvmOpts = "-Xms2G -Xmx4G";
 
-          symlinks = {
-            "mods" = ./mods;
-          };
-        };
+        #  symlinks = {
+        #    "mods" = pkgs.linkFarmFromDrvs "mods" (
+        #  builtins.attrValues {
+        #    Fabric-API = pkgs.fetchurl {
+        #      url = "https://cdn.modrinth.com/data/P7dR8mSH/versions/5zJNhXV2/fabric-api-0.141.4%2B1.21.11.jar";
+        #      sha512 ="c092d48c6453bec3264f80f6a35bb334aba3112b5cd6c0e0b2676ce4d81e702cb1e522337f3a732348e757cc2226da3c601a314ae8766334f16af71a13bcc98d";
+        #    };
+        #  }
+        #  );
+        #};
       };
     };
+  };
 }
