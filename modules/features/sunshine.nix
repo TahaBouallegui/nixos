207fc81ebb860466081486ea157cc50e1b7449aa
Za3ter <ahmedtbou@tutamail.com>
2026-07-11 18:28:14 +0200

    rdp

--- modules/features/sunshine.nix
+++ modules/features/sunshine.nix
@@ -1,72 +1,38 @@
-{
-  self,
-  inputs,
-  ...
-}: {
-  flake.nixosModules.sunshine = {
-    config,
-    pkgs,
-    ...
-  }: {
-
-    # X11 display with i3 and auto‑login
-    services.xserver = {
-      enable = true;
-      desktopManager.xfce.enable = true;
-      displayManager.lightdm.enable = true;
-      displayManager.autoLogin = {
+{ self, inputs, ... }: {
+  flake.nixosModules.sunshine =
+    {
+      config,
+      pkgs,
+      ...
+    }:
+    {
+      services.xserver = {
         enable = true;
-        user = "za3ter";
+        desktopManager.gnome3.enable = true;
+        displayManager.gdm.enable = true;
       };
-    };
-#    environment.etc."X11/xorg.conf.d/headless-virtual-display.conf".text = ''
-#  Section "ServerLayout"
-#      Identifier "TwinLayout"
-#      Screen 0 "metaScreen" 0 0
-#  EndSection
-#
-#  Section "Monitor"
-#      Identifier "Monitor0"
-#      Option "Enable" "true"
-#  EndSection
-#
-#  Section "Device"
-#      Identifier "Card0"
-#      Driver "nvidia"
-#      VendorName "NVIDIA Corporation"
-#      Option "MetaModes" "1920x1080"
-#      Option "ConnectedMonitor" "DP-0"
-#      Option "ModeValidation" "NoDFPNativeResolutionCheck,NoVirtualSizeCheck,NoMaxPClkCheck,NoHorizSyncCheck,NoVertRefreshCheck,NoWidthAlignmentCheck"
-#  EndSection
-#
-#  Section "Screen"
-#      Identifier "metaScreen"
-#      Device "Card0"
-#      Monitor "Monitor0"
-#      DefaultDepth 24
-#      Option "MetaModes" "True"
-#      SubSection "Display"
-#          Modes "1920x1080"
-#      EndSubSection
-#  EndSection
-#'';
-users.users.za3ter = {
-  extraGroups = [ "uinput" ];
-};
-    hardware.uinput.enable = true;
 
-    services.sunshine = {
-      enable = true;
-      autoStart = true; 
-      openFirewall = true;
-      package = pkgs.sunshine.override {
-        cudaSupport = true;
+      services.xrdp.enable = true;
+      services.gnome.gnome-remote-desktop.enable = true;
+      services.xrdp.defaultWindowManager = "${pkgs.gnome-session}/bin/gnome-session";
+      services.xrdp.openFirewall = true;
+
+      hardware.uinput.enable = true;
+      users.users.za3ter.extraGroups = [ "uinput" ];
+
+      services.sunshine = {
+        enable = true;
+        autoStart = true;
+        openFirewall = true;
+        package = pkgs.sunshine.override {
+          cudaSupport = true;
+        };
       };
-    };
 
-    programs.steam = {
+      programs.steam = {
         enable = true;
         extraCompatPackages = [ pkgs.dwproton-bin ];
+      };
+
     };
-  };
 }
