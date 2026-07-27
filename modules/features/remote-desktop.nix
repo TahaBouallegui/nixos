7617e9bb58ba3a098684f6cbd120cae08f218445
Za3ter <ahmedtbou@tutamail.com>
2026-07-27 20:48:52 +0200

    Autologin

--- modules/features/remote-desktop.nix
+++ modules/features/remote-desktop.nix
@@ -6,8 +6,7 @@
       services.xrdp.enable = true;
       services.xrdp.defaultWindowManager = "${pkgs.gnome-session}/bin/gnome-session"; # gnome wayland session
       services.gnome.gnome-remote-desktop.enable = true; # needs gnome-remote-desktop backend to work!!
-      services.displayManager.autoLogin.enable = false;
-      services.getty.autologinUser = null;
+      services.displayManager.autoLogin.enable = true;
       networking.firewall.allowedTCPPorts = [ 3389 ];
     };
 }
