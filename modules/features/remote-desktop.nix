985340b7d268b4b85dafd7fb7d793f97190f5868
Za3ter <ahmedtbou@tutamail.com>
2026-07-27 21:57:05 +0200

    vnc...

--- modules/features/remote-desktop.nix
+++ modules/features/remote-desktop.nix
@@ -4,24 +4,16 @@
     { pkgs, config, ... }:
     {
 
-      # Enable the X server and GNOME
       services.xserver.enable = true;
+      services.xserver.displayManager.gdm.enable = true;
       services.xserver.desktopManager.gnome.enable = true;
 
-      # Enable TigerVNC server
-      services.tigervnc = {
-        enable = true;
-        display = 1; # Uses :1 (port 5901)
-        geometry = "1920x1080"; # Adjust to your client resolution
-        depth = 24;
-        passwordFile = "/etc/vnc/passwd"; # File containing the VNC password
+      services.gnome.gnome-remote-desktop.enable = true;
 
-        # Optional: custom session command (GNOME is default if not set)
-        # session = "gnome-session";
-      };
+      services.xrdp.enable = true;
+      services.xrdp.defaultWindowManager = "gnome-session";
+      services.xrdp.openFirewall = true; 
 
-      # Open the firewall for the VNC port
-      networking.firewall.allowedTCPPorts = [ 5901 ];
       systemd.sleep.settings.Sleep = {
         AllowSuspend = "no";
         AllowHibernation = "no";
