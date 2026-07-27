d6338d5b422515b9f16d5bcccadea063ee03df13
Za3ter <ahmedtbou@tutamail.com>
2026-07-27 20:55:15 +0200

    maybe now it works?

--- modules/features/remote-desktop.nix
+++ modules/features/remote-desktop.nix
@@ -6,7 +6,11 @@
       services.xrdp.enable = true;
       services.xrdp.defaultWindowManager = "${pkgs.gnome-session}/bin/gnome-session"; # gnome wayland session
       services.gnome.gnome-remote-desktop.enable = true; # needs gnome-remote-desktop backend to work!!
-      services.displayManager.autoLogin.enable = true;
+      services.displayManager.autoLogin.enable = false;
+      services.getty.autologinUser = null;
       networking.firewall.allowedTCPPorts = [ 3389 ];
+      networking.firewall.allowedUDPPorts = [ 3389 ];
+
+
     };
 }
