bd8d575dbd69ee31ec36f9cfb7be618d838d46f2
Za3ter <ahmedtbou@tutamail.com>
2026-07-27 14:56:58 +0200

    monkey changes

--- modules/features/moise.nix
+++ modules/features/moise.nix
@@ -1,6 +1,21 @@
 {
-  flake.nixosModules.faker = {pkgs-stable, ...}: {
-        
+  flake.nixosModules.faker =
+    { pkgs, config, ... }:
+    {
+      services.immich = {
+        enable = true;
+        port = 2283;
+        host = "0.0.0.0";
+        openFirewall = true;
+        mediaLocation = "/var/lib/immich";
+
+        # `null` gives access to all devices, including /dev/nvidia* nodes
+        accelerationDevices = null;
+      };
+
+      users.users.immich.extraGroups = [
+        "video"
+        "render"
+      ];
     };
 }
-
