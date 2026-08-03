5919ee22110f5e5110af1b032aca807a989a1431
Za3ter <ahmedtbou@tutamail.com>
2026-08-03 12:31:48 +0200

    git oopsie?

--- modules/features/mincraft-server/minecraft.nix
+++ modules/features/mincraft-server/minecraft.nix
@@ -12,10 +12,18 @@
       flux = {
         enable = true;
         servers.halalserver = {
-          enable = true;
+          enable = false;
           package = pkgs.mkMinecraftServer {
             name = "HAHALminecraftserver";
             src = ./mcman;
+            hash = "sha256-5DZyuiBqemIDVSyIjWJB1Qogg+hlIbQq1S+Ku1Eg/Tw=";
+          };
+        };
+        servers.halaltensura = {
+          enable = true;
+          package = pkgs.mkMinecraftServer {
+            name = "HAHALtensura";
+            src = ./mcmantensura;
             hash = "sha256-5DZyuiBqemIDVSyIjWJB1Qogg+hlIbQq1S+Ku1Eg/Tw=";
           };
         };
