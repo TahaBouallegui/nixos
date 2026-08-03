5b15189bd79ce71066e3e5887e1a5325981b4a97
Za3ter <ahmedtbou@tutamail.com>
2026-08-03 13:08:10 +0200

    minecraft hashj

--- modules/features/mincraft-server/minecraft.nix
+++ modules/features/mincraft-server/minecraft.nix
@@ -24,7 +24,7 @@
           package = pkgs.mkMinecraftServer {
             name = "HAHALtensura";
             src = ./mcmantensura;
-            hash = "sha256-5DZyuiBqemIDVSyIjWJB1Qogg+hlIbQq1S+Ku1Eg/Tw=";
+            hash = "sha256-mABEsPEW6JuUUA2RKPC+ZHGQeGkf8ClKZMGA1R2qT0Q=";
           };
         };
       };
