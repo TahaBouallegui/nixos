ad8e2e57912d7c3c0bb160515ba8285548233750
Za3ter <ahmedtbou@tutamail.com>
2026-08-03 12:22:19 +0200

    grocy and minecraft

--- modules/features/grocy.nix
+++ modules/features/grocy.nix
@@ -6,11 +6,18 @@
       security.acme.acceptTerms = true;
       services.grocy = {
         enable = true;
-        hostName = "grocy.tld";
+        hostName = "grocy.ltd";
         settings = {
           currency = "EUR";
           culture = "fr";
         };
       };
+      services.nginx.virtualHosts."grocy.ltd".listen = [
+        {
+          addr = "100.68.187.8";
+          port = 8000;
+        }
+      ];
+      networking.firewall.allowedTCPPorts = [ 8000 ];
     };
 }
